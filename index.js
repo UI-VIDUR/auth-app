850abf74df40c8cfb4905c118a3bad653ed62865
vidur-believintech <vidurtuli53@gmail.com>
2024-06-24 01:08:12 +0530

    solved the cors problem

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const cors = require('cors');
 const connectDB = require("./connection");
 const PORT = 3001;
 const app = express();
 const userAuth = require("./routes/auth");
 
+// Configure CORS options
+const corsOptions = {
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 /** Db connection */
 connectDB();
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
